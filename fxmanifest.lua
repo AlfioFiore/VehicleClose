fdc8d7e2a2ff3f375860383a08bb081fa0c6fe8e
Alfonso Fiorentino <alfionuovo@gmail.com>
2023-07-01 14:15:51 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+
+-- Imposta il nome e l'autore dello script
+name 'VehicleSpawn'
+author 'AlfonsoFiorentino'
+
+-- Specifica la versione del tuo script
+version '1.0.0'
+
+-- Specifica la cartella in cui si trova il tuo script
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Dichiarazione dei file di script
+client_scripts {
+    'main.lua',
+}
